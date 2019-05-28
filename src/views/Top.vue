fffbca0ed147a98159f53d1c0480104d73f1e46b
nkn-ms <nkn-ms@hotmail.co.jp>
2019-05-28 20:19:02 +0900

    delete useless code

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -17,11 +17,6 @@ export default {
     ScHeader,
     ScContents
   },
-  data () {
-    return {
-      loading: false
-    }
-  },
   computed: {
     isScrapingData () {
       return this.$store.state.scrapingData !== null
@@ -31,39 +26,6 @@ export default {
     setScrapingData (data) {
       this.$store.dispatch('setScrapingData', data)
       console.log(this.$store.state)
-    },
-    // onclickでスクレイピング実行
-    getScraping () {
-      console.log('getScraping')
-      this.loading = true
-      let scrapingData = ''
-      const decoder = new TextDecoder()
-      fetch('https://heroku-puppeteer-note.herokuapp.com/scraping', {
-        mode: 'cors'
-      }).then((response) => {
-        console.log(response)
-        return response.body.getReader()
-      }).then((reader) => {
-        // ReadableStream.read()はPromiseを返す。
-        // Promiseは{ done, value }として解決される。
-        // データを読み込んだとき：doneはfalse, valueは値。
-        // データを読み込み終わったとき：doneはtrue, valueはundefined。
-        function readChunk ({ done, value }) {
-          if (done) {
-            // 読み込みが終わっていれば最終的なテキストを表示する。
-            console.log(scrapingData)
-            return
-          }
-          scrapingData += decoder.decode(value)
-          // 次の値を読みにいく。
-          reader.read().then(readChunk)
-        }
-        // 最初の値を読み込む。
-        reader.read().then(readChunk)
-      }).then(() => {
-        this.setScrapingData(JSON.parse(scrapingData))
-        this.loading = false
-      })
     }
   }
 }
